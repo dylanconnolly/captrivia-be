58f78798f6608c38bd742548faa714dc6c68226c
Dylan Connolly <dylanconnolly145@gmail.com>
2024-08-06 13:51:27 -0600

    move game logic to captrivia package

--- captrivia/game.go
+++ captrivia/game.go
@@ -1,6 +1,8 @@
 package captrivia
 
 import (
+	"encoding/json"
+
 	"github.com/google/uuid"
 )
 
@@ -14,21 +16,81 @@ const (
 type Game struct {
 	ID            uuid.UUID       `json:"id"`
 	Name          string          `json:"name"`
-	Players       []string        `json:"players"`
 	PlayersReady  map[string]bool `json:"players_ready"`
 	PlayerCount   int             `json:"player_count"`
 	QuestionCount int             `json:"question_count"`
 	State         string          `json:"state"`
+
+	questions            []Question
+	currentQuestionIndex int
+	scores               map[string]int
 }
 
-func NewGame(id uuid.UUID, name string, players []string, pReady map[string]bool, qCount int, state string) *Game {
-	return &Game{
-		ID:            id,
-		Name:          name,
-		Players:       players,
-		PlayersReady:  pReady,
-		PlayerCount:   len(players),
-		QuestionCount: qCount,
-		State:         state,
+type PlayerScore struct {
+	Player string `json:"player"`
+	Score  int    `json:"score"`
+}
+
+func (g Game) MarshalJSON() ([]byte, error) {
+	type Alias Game
+	return json.Marshal(&struct {
+		Players []string `json:"players"`
+		Alias
+	}{
+		Players: g.PlayerNames(),
+		Alias:   (Alias)(g),
+	})
+}
+
+func (g Game) PlayerNames() []string {
+	names := make([]string, 0, len(g.PlayersReady))
+	for name := range g.PlayersReady {
+		names = append(names, name)
 	}
+
+	return names
+}
+
+func NewGame(name string, qCount int) *Game {
+	return &Game{
+		ID:            uuid.New(),
+		Name:          name,
+		PlayersReady:  make(map[string]bool),
+		QuestionCount: qCount,
+		State:         GameStateWaiting,
+		scores:        make(map[string]int),
+	}
+}
+
+func (g *Game) AddPlayer(player string) {
+	g.PlayersReady[player] = false
+	g.scores[player] = 0
+	g.PlayerCount++
+}
+
+func (g *Game) RemovePlayer(player string) {
+	delete(g.PlayersReady, player)
+	delete(g.scores, player)
+	g.PlayerCount--
+}
+
+func (g *Game) AddQuestion(q Question) {
+	g.questions = append(g.questions, q)
+}
+
+func (g *Game) PlayerScores() []PlayerScore {
+	var playerScores []PlayerScore
+	for player, score := range g.scores {
+		s := PlayerScore{
+			Player: player,
+			Score:  score,
+		}
+		playerScores = append(playerScores, s)
+	}
+
+	return playerScores
+}
+
+func (g *Game) StartGame() {
+	g.State = GameStateCountdown
 }
---
+++ captrivia/game_test.go
@@ -0,0 +1,100 @@
+package captrivia_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/dylanconnolly/captrivia-be/captrivia"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	gameName      = "test game"
+	questionCount = 5
+)
+
+func CreateTestGame() captrivia.Game {
+	return *captrivia.NewGame(gameName, questionCount)
+}
+
+func TestNewGame(t *testing.T) {
+	g := captrivia.NewGame(gameName, questionCount)
+
+	assert.Equal(t, gameName, g.Name)
+	assert.Equal(t, questionCount, g.QuestionCount)
+	assert.Equal(t, captrivia.GameStateWaiting, g.State)
+	assert.Equal(t, 0, g.PlayerCount)
+	assert.IsType(t, make(map[string]bool), g.PlayersReady)
+	assert.IsType(t, make(map[string]bool), g.PlayersReady)
+	assert.IsType(t, make(map[string]bool), g.PlayersReady)
+}
+
+func TestMarshalJSON(t *testing.T) {
+	g := CreateTestGame()
+
+	bytes, err := json.Marshal(g)
+
+	if !assert.NoError(t, err) {
+		t.Error("error marshalling game: ", err)
+	}
+
+	var resp map[string]interface{}
+	json.Unmarshal(bytes, &resp)
+	assert.Contains(t, resp, "id")
+	assert.Contains(t, resp, "name")
+	assert.Contains(t, resp, "players")
+	assert.Contains(t, resp, "players_ready")
+	assert.Contains(t, resp, "question_count")
+	assert.Contains(t, resp, "state")
+	assert.NotContains(t, resp, "Scores")
+	assert.NotContains(t, resp, "Questions")
+}
+
+func TestPlayerNames(t *testing.T) {
+	g := CreateTestGame()
+	g.PlayersReady["test1"] = false
+	g.PlayersReady["test2"] = false
+	expected := []string{"test1", "test2"}
+	players := g.PlayerNames()
+
+	assert.Equal(t, expected, players)
+}
+
+func TestPlayerScores(t *testing.T) {
+	g := CreateTestGame()
+
+	g.AddPlayer("player 1")
+
+	assert.Equal(t, 1, len(g.PlayerScores()))
+	assert.Equal(t, "player 1", g.PlayerScores()[0].Player)
+	assert.Equal(t, 0, g.PlayerScores()[0].Score)
+
+	g.AddPlayer("player 2")
+	assert.Equal(t, 2, len(g.PlayerScores()))
+}
+
+func TestAddPlayer(t *testing.T) {
+	g := CreateTestGame()
+
+	assert.Empty(t, g.PlayersReady)
+	g.AddPlayer("test player")
+
+	assert.NotEmpty(t, g.PlayersReady)
+	assert.NotEmpty(t, g.PlayerScores())
+	assert.Equal(t, 1, g.PlayerCount)
+
+	g.AddPlayer("test player 2")
+	assert.Equal(t, 2, g.PlayerCount)
+}
+
+func TestRemovePlayer(t *testing.T) {
+	g := CreateTestGame()
+
+	g.AddPlayer("test player")
+	g.RemovePlayer("test player")
+
+	assert.Empty(t, g.PlayersReady)
+
+	assert.Equal(t, 0, g.PlayerCount)
+	assert.Empty(t, 0, g.PlayerScores)
+}
